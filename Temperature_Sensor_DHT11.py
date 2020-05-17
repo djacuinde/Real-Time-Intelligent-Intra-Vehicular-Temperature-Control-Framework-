705cf0aacacf20f3223bcfb7b8642259fd766eac
djacuinde96 <54961892+djacuinde96@users.noreply.github.com>
2020-05-16 18:40:10 -0700

    Add files via upload

---
+++ Temperature_Sensor_DHT11.py
@@ -0,0 +1,25 @@
+
+# git clone https://github.com/adafruit/Adafruit_Python_DHT.git
+# cd Adafruit_Python_DHT
+# sudo apt-get install build-essential python-dev
+# sudo python setup.py install
+
+import sys
+import Adafruit_DHT
+
+DHT11_temperature = 0.0
+DHT11_humidity = 0.0
+
+while True:
+    DHT11_humidity, DHT11_temperature = Adafruit_DHT.read_retry (11,4) #(sensor, pin)
+    CurrentTemp = Get_Temperature("F", DHT11_temperature)
+    print 'Temperature: {0:0.1f} C Humidity: {1:0.1f} %'.format(CurrentTemp, DHT11_humidity)
+
+# temp  = F for Fahrenheit
+# temp  = C for Celsius
+def Get_Temperature(temp, DHT11_temperature )
+    if temp = "F"
+        temperature = 9/5.0 * DHT11_temperature + 32
+    else if temp = "C"
+        tempertaure = 5.0/9 * (DHT11_temperature - 32)
+    return  temperature
