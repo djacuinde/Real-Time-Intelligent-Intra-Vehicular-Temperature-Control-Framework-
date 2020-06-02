ccb6140b3cabf555ea4a8ed9e6873a585f3af26a
djacuinde96 <54961892+djacuinde96@users.noreply.github.com>
2020-06-02 15:59:14 -0700

    Update Temperature_Sensor_DHT11.py

--- Temperature_Sensor_DHT11.py
+++ Temperature_Sensor_DHT11.py
@@ -23,3 +23,21 @@ def Get_Temperature(temp, DHT11_temperature)
     else if temp = "C"
         temperature = 5.0/9 * (DHT11_temperature - 32)
     return  temperature
+
+
+
+#sudo pip3 install Adafruit_DHT
+
+import Adafruit_DHT
+import time
+ 
+DHT_SENSOR = Adafruit_DHT.DHT11
+DHT_PIN = 4
+ 
+while True:
+    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+    if humidity is not None and temperature is not None:
+        print("Temp={0:0.1f}C Humidity={1:0.1f}%".format(temperature, humidity))
+    else:
+        print("Sensor failure. Check wiring.");
+    time.sleep(3);
