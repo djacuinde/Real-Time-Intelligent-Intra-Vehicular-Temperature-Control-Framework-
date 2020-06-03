9fd5b1512700e149c9824f34949f4464038e1c7f
djacuinde96 <54961892+djacuinde96@users.noreply.github.com>
2020-06-03 12:50:37 -0700

    Update Temperature_Sensor_DHT11.py

--- Temperature_Sensor_DHT11.py
+++ Temperature_Sensor_DHT11.py
@@ -1,43 +1,19 @@
-
-# git clone https://github.com/adafruit/Adafruit_Python_DHT.git
-# cd Adafruit_Python_DHT
-# sudo apt-get install build-essential python-dev
-# sudo python setup.py install
-
-import sys
-import Adafruit_DHT
-
-DHT11_temperature = 0.0
-DHT11_humidity = 0.0
-
-while True:
-    DHT11_humidity, DHT11_temperature = Adafruit_DHT.read_retry (11,4) #(sensor, pin)
-    CurrentTemp = Get_Temperature("F", DHT11_temperature)
-    print 'Temperature: {0:0.1f} C Humidity: {1:0.1f} %'.format(CurrentTemp, DHT11_humidity)
-
-# temp  = F for Fahrenheit
-# temp  = C for Celsius
-def Get_Temperature(temp, DHT11_temperature)
-    if temp = "F"
-        temperature = 9/5.0 * DHT11_temperature + 32
-    else if temp = "C"
-        temperature = 5.0/9 * (DHT11_temperature - 32)
-    return  temperature
-
-
-
 #sudo pip3 install Adafruit_DHT
 
 import Adafruit_DHT
 import time
- 
-DHT_SENSOR = Adafruit_DHT.DHT11
-DHT_PIN = 4
- 
+
+DHT_SENSOR = Adafruit_DHT.DHT11 #Type of sensor (DTH11 or DTH22)
+DHT_PIN = 4 #GPIO Pin Number
+Degree = 'F' #F = Fahrenheit, C = Celsius 
+
 while True:
     humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
     if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}C Humidity={1:0.1f}%".format(temperature, humidity))
+        if Degree is 'F':
+            print("Temp={0:0.1f}F Humidity={1:0.1f}%".format(9/5.0*temperature+32, humidity))
+        if Degree is 'C':
+            print("Temp={0:0.1f}C Humidity={1:0.1f}%".format(temperature, humidity))
     else:
         print("Sensor failure. Check wiring.");
-    time.sleep(3);
+    time.sleep(3);#delay of 3
