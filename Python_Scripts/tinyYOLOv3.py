37662eab112ed6155debfef572327ae06864cf4a
djacuinde96 <54961892+djacuinde96@users.noreply.github.com>
2020-06-15 12:25:59 -0700

    Update tinyYOLOv3.py

--- Python_Scripts/tinyYOLOv3.py
+++ Python_Scripts/tinyYOLOv3.py
@@ -6,7 +6,7 @@
 #Run the command below using Command Line using pi Camera
 # $ python detect_realtime_tinyyolo_ncs.py --conf config/config.json 
 
-#Script was developed by Adrian Rosebrock from pyimagesearch
+# Script was developed by Adrian Rosebrock
 # Modified by Daniel Jacuinde from CSU, Fresno 
 
 ####################################Libraries########################################
