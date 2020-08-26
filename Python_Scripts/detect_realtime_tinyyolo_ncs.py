1e0d7c00b1436c9cc28457de0036158b6f00497b
djacuinde96 <54961892+djacuinde96@users.noreply.github.com>
2020-08-26 14:53:15 -0700

    Update detect_realtime_tinyyolo_ncs.py

--- Python_Scripts/detect_realtime_tinyyolo_ncs.py
+++ Python_Scripts/detect_realtime_tinyyolo_ncs.py
@@ -6,6 +6,7 @@
 #Run the command below using Command Line using pi Camera
 # $ python detect_realtime_tinyyolo_ncs.py --conf config/config.json 
 
+# Developed by Adrian Rosebrock
 # Modified by Daniel Jacuinde from CSU, Fresno 
 
 ####################################Libraries########################################
