8b52814890a2ff517e44e89b259e6b639ca7be65
djacuinde96 <54961892+djacuinde96@users.noreply.github.com>
2020-09-13 15:59:00 -0700

    Update NOTIFICATION.py

--- Python_Scripts/NOTIFICATION.py
+++ Python_Scripts/NOTIFICATION.py
@@ -4,31 +4,27 @@ import smtplib
 from cryptography.fernet import Fernet
 
 #EMAIL_PASSWORD key
-cs = Fernet(b'BVeJdI_IyCFvwedgYx7QeCUVFiWC-cZW7pcJWpD6yo8=')
+cs = Fernet(b'HqiwPbvnbVK5Yshgop7xOKxLCFyiPaF6qFSjxW4i2DM=')
 #EMAIL_PASSWORD cipher password
-ct = b'gAAAAABfDhfuj5exwWLvrXrIZWEwXsafl58DedStR7mTw_fZKdMp2jzfMQ7iY2G03jg-vXEpmKkRHGW_flv6FngfRX--o7-6Fg==' 
+ct = b'gAAAAABfXp6RDdZKMrmaMB6zJ-t8nDI3M1-tnlH8GiMSjgZebQRQA6Au3oPH7RerqMWw8UhMPL0tAfxCkVV5iE_QJT65TMkurw==' 
 
+#SMS ending
 carriers ={
-    'a' or 'att' : '@txt.att.net',
-    'b' or 'tmobile' : '@tmomail.net',
-    'c' or 'verizon': '@vtext.com',
-    'd' or 'sprint' : '@page.nextel.com'  #'@messaging.sprintpcs.com'
+    'att' : '@txt.att.net',
+    'tmobile' : '@tmomail.net',
+    'verizon': '@vtext.com',
+    'sprint' : '@page.nextel.com'  #'@messaging.sprintpcs.com'
     }
 
 SMTP_SERVER = 'smtp.gmail.com' #Email Server 
 SMTP_PORT = 587 #Server Port 
 GMAIL_USERNAME = 'savinglife2020fresnostate@gmail.com' #gmail account of SENDER
-SUBJECT = "PVH Prevention Service"
-MESSAGE = "There is an unattended PET or CHILD is your vehicle!"
-
-#PHONE_NUM = '5597045940{}'.format (carriers['att']) #Pet's owner phone number ####encrypt number?####
-PHONE_NUM = 'DAFAULT';#?
-RECIPIENT = 'DEFAULT';
+SUBJECT = " PVH Prevention Service"
+MESSAGE = " There is an unattended PET or CHILD is your vehicle!"
 
 
 class Notification:
     def sendNotification(RECIPIENT, phoneNumber, carrierType):
-        if RECIPIENT != 'DEFAULT':
         
             #Create Headers
             headers = ["From: " + GMAIL_USERNAME, "Subject: " + SUBJECT, "To: " + RECIPIENT ,
@@ -42,18 +38,17 @@ class Notification:
             session.ehlo()
             
             #Login to Gmail
-            #session.login(GMAIL_USERNAME, bytes(cs.decrypt(ct)).decode("utf-8"))
-            session.login('d.jacuinde96@gmail.com', 'Greenship_582.')
+            session.login(GMAIL_USERNAME, bytes(cs.decrypt(ct)).decode("utf-8"))
+            #session.login('d.jacuinde96@gmail.com', 'Greenship_582.')
      
             #Send Email & SMS then Exit
             session.sendmail(GMAIL_USERNAME, RECIPIENT , headers + "\r\n\r\n" + MESSAGE) #email
-            
-            PHONE_NUM = phoneNumber + '{}'.format(carriers[carrierType]) 
-            if PHONE_NUM != 'DEFAULT' : 
-                session.sendmail(GMAIL_USERNAME, PHONE_NUM, MESSAGE)#sms
-                
+            PHONE_NUM = phoneNumber + '{}'.format(carriers[carrierType])  
+            session.sendmail(GMAIL_USERNAME, PHONE_NUM, MESSAGE)#sms 
             session.quit
-        
- 
+            
+if __name__ == '__main__':
+    N = Notification
+    N.sendNotification('djacuinde96@gmail.com', '5597045940', 'att')
 
  
