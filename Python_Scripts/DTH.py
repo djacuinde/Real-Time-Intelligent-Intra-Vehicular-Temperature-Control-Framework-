f0ec93062d88798946ac5a5217d018470a788085
djacuinde96 <54961892+djacuinde96@users.noreply.github.com>
2020-09-29 12:04:13 -0700

    Update DTH.py

--- Python_Scripts/DTH.py
+++ Python_Scripts/DTH.py
@@ -9,10 +9,10 @@ DHT_SENSOR = Adafruit_DHT.DHT22 #Type of sensor (DTH11 or DTH22)
 DHT_PIN = 4 #GPIO Pin Number
 
 def readDTH(Degree):
-    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)#Get sensor reading
     if humidity is not None and temperature is not None:
-        now = datetime.now()
-        current_time = now.strftime ("%I:%M:%S %p")
+        now = datetime.now()    
+        current_time = now.strftime ("%I:%M:%S %p") #output time to format HH:MM:SS AM/PM
         print ("Time =", current_time)
         if Degree is 'F':
             print("Temp = {0:0.1f} F ; Humidity = {1:0.1f}%".format(9/5.0*temperature+32, humidity))
