f70d512688c2d87d4c51cfddefca8f5d305e29c2
djacuinde96 <54961892+djacuinde96@users.noreply.github.com>
2020-09-29 14:59:36 -0700

    Update EMAIL_VALID.py

--- Python_Scripts/EMAIL_VALID.py
+++ Python_Scripts/EMAIL_VALID.py
@@ -4,14 +4,12 @@
 
 import re as valid
 
-# Make a regular expression 
-# for validating an Email
+# Make a regular expression for validating an Email
 regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
 # for custom mails use: '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w+$'
 
 def check(email):
-    # pass the regular expression 
-    # and the string in search() method 
+    # Check for email validation
     if(valid.search(regex,email)):  
         return True
     else:  
