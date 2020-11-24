0b10cb45a1c6b3fccbfe7ab32692b5e1f98a2ee4
Daniel Jacuinde <54961892+djacuinde96@users.noreply.github.com>
2020-11-23 18:46:45 -0800

    Update DTH.py

--- Python_Scripts/DTH.py
+++ Python_Scripts/DTH.py
@@ -1,30 +1,21 @@
-
 #sudo pip3 install Adafruit_DHT
 
-import Adafruit_DHT
+import board
+import adafruit_dht
 import time
 from datetime import datetime
 
-DHT_SENSOR = Adafruit_DHT.DHT22 #Type of sensor (DTH11 or DTH22)
-DHT_PIN = 4 #GPIO Pin Number that is being utilized on the Raspberry Pi 
+#DTH22 with data_pin on GPIO4 on Pi
+dht_device = adafruit_dht.DHT22(board.D4, use_pulseio=False)
 
-def readDTH(Degree):
-    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)#Get sensor reading
-    if humidity is not None and temperature is not None:
-        now = datetime.now()    
-        current_time = now.strftime ("%I:%M:%S %p") #output time to format HH:MM:SS AM/PM
-        print ("Time =", current_time)
-        if Degree is 'F':
-            print("Temp = {0:0.1f} F ; Humidity = {1:0.1f}%".format(9/5.0*temperature+32, humidity))
-        if Degree is 'C':
-            print("Temp = {0:0.1f} C ; Humidity = {1:0.1f}%".format(temperature, humidity))
-    else:
-        print("Cannot read sensor or lost connection !")
-        time.sleep(1)
-    time.sleep(2)
+def readDTH():
+    temperature = dht_device.temperature
+    now = datetime.now()    
+    current_time = now.strftime ("%I:%M:%S %p") #output time to format HH:MM:SS AM/PM
+    print ("Time =", current_time)
+    print("Temp = {0:0.1f} C ".format(temperature))
+    return temperature
     
 if __name__ == '__main__':
-    readDTH('F')
-
-    
-    
+    Temp = readDTH()
+    print(Temp)
